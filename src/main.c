8e8875e52b30e0284dcd47a4d3fcabad8a43f4f3
han-nwin <hannguyen.win@gmail.com>
2024-11-24 00:20:16 -0600

    a

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <getopt.h>
+#include <string.h>
 
 #include "common.h"
 #include "file.h"
@@ -16,19 +18,28 @@ void print_usage(char *argv[]) {
 int main (int argc, char *argv[]) {
 
   char *filepath = NULL;
+  char *addstring = NULL;
   bool newfile = false;
   int c;
 
   int db_fd = -1;
+  struct employee_t *employees = NULL;
   struct dbheader_t *db_hd = NULL;
+  bool list = false;
 
-  while ((c = getopt(argc, argv, "nf:")) != -1) {
+  while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
     switch (c) {
       case 'n':
         newfile = true;
         break;
       case 'f':
         filepath = optarg;
+        break;
+      case 'a':
+        addstring = optarg;
+        break;
+      case 'l':
+        list = true;
         break;
       case '?':
         printf("Unknown option -%c\n", c);
@@ -73,11 +84,30 @@ int main (int argc, char *argv[]) {
     }
   }
 
+  if (read_employees(db_fd, db_hd, &employees) != STATUS_SUCCESS) {
+    printf("Failed to read employees\n");
+    return -1;
+  }
+
+  if (addstring) {
+    db_hd->count++;
+    employees = realloc(employees, db_hd->count*sizeof(struct employee_t));
+    if (add_employee(db_hd, employees, addstring) != STATUS_SUCCESS) {
+      return -1;
+    };
+  }
+
+  if (list) {
+    list_employees(db_hd, employees);
+  }
+
+  printf("------------------------\n");
   printf("Newfile: %d\n", newfile);
   printf("Filepath: %s\n", filepath);
 
   //Write to db file
-  output_file(db_fd, db_hd);
+  output_file(db_fd, db_hd, employees);
 
   return 0;
+
 }
--- src/parse.c
+++ src/parse.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #include "parse.h"
 #include "common.h"
@@ -92,14 +93,15 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
  * @brief Take an output on disk file descriptor and write the content of the file
  *
  * */
-int output_file(int fd, struct dbheader_t *db_hd) {
+int output_file(int fd, struct dbheader_t *db_hd, struct employee_t *employees) {
   if (fd < 0) {
     printf("Got a bad file descriptor from user\n");
     return STATUS_ERROR;
   }
+  int realcount = db_hd->count; //reserve the host data byte sequence
 
   db_hd->magic = htonl(db_hd->magic);
-  db_hd->filesize = htonl(db_hd->filesize);
+  db_hd->filesize = htonl(sizeof(struct dbheader_t) + sizeof(struct employee_t)*realcount);
   db_hd->version = htons(db_hd->version);
   db_hd->count = htons(db_hd->count);
 
@@ -107,7 +109,76 @@ int output_file(int fd, struct dbheader_t *db_hd) {
 
   write(fd, db_hd, sizeof(struct dbheader_t));
 
+  int i = 0;
+  for (; i < realcount; i++) {
+    employees[i].hours = htonl(employees[i].hours);
+    write(fd, &employees[i], sizeof(struct employee_t));
+  }
+
   return STATUS_SUCCESS;
 
 }
 
+
+/**
+ * @brief Read employee structure data
+ * */
+int read_employees(int fd, struct dbheader_t *db_hd, struct employee_t **employeesOut) {
+  if (fd < 0) {
+    printf("Got a bad file descriptor from user \n");
+    return STATUS_ERROR;
+  }
+
+  int count = db_hd->count;
+
+  struct employee_t *employees = calloc(count, sizeof(struct employee_t));
+  if (employees == NULL) {
+    printf("Calloc failed \n");
+    return STATUS_ERROR;
+  }
+
+  read(fd, employees, count*sizeof(struct employee_t));
+
+  int i = 0;
+  for (; i < count; i++) {
+    employees[i].hours = ntohl(employees[i].hours);
+  }
+
+  *employeesOut = employees;
+   return STATUS_SUCCESS;
+
+}
+
+/**
+ * @brief Function to add employees to db 
+ * */
+int add_employee(struct dbheader_t *db_hd, struct employee_t *employees, char *addstring) {
+
+  char *name = strtok(addstring, ",");
+  char *address = strtok(NULL, ",");
+  char *hours = strtok(NULL, ",");
+
+  printf("%s | %s | %s\n", name, address, hours);
+
+  strncpy(employees[db_hd->count - 1].name, name, sizeof(employees[db_hd->count - 1].name));
+  strncpy(employees[db_hd->count - 1].address, address, sizeof(employees[db_hd->count - 1].address));
+  employees[db_hd->count - 1].hours = atoi(hours);
+
+
+  return STATUS_SUCCESS;
+}
+
+
+
+/**
+ * @brief List employees to stdin
+ * */
+void list_employees(struct dbheader_t *db_hd, struct employee_t *employees) {
+  int i = 0;
+  for (; i < db_hd->count; i++) {
+    printf("\nEmployee: %d\n", i);
+    printf("Name: %s\n", employees[i].name);
+    printf("Address: %s\n", employees[i].address);
+    printf("Hours: %d\n", employees[i].hours);
+  }
+}
